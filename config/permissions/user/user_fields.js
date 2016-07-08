c41f9c6fa970f4fc597e0fb951ceee221a3d16cf
grodighiero <giovanni.rodighiero@develon.com>
2016-07-08 12:04:30 +0200

    rigenerated project

--- config/permissions/user/user_fields.js
+++ config/permissions/user/user_fields.js
@@ -1,26 +1,26 @@
 module.exports.user = {
   fields:{
-    
+
       password: function(page, user){
-        return true;
+        return (page === 'new');
       },
-    
+
       role: function(page, user){
         return true;
       },
-    
+
       email: function(page, user){
         return true;
       },
-    
-    
+
+
       website: function(page, user){
         return true;
       },
-    
+
       description: function(page, user){
         return true;
       },
-    
+
   }
 }
